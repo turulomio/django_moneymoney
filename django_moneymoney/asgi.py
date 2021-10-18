f54866265733164a949342c0dac1475e195367c0
turulomio <turulomio@yahoo.es>
2021-10-18 08:20:26 +0200

    Check for database update on each run

--- django_moneymoney/asgi.py
+++ django_moneymoney/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from django.conf import settings
+from moneymoney.reusing.connection_pg import Connection
+from moneymoney.database_update import  database_update
+from moneymoney import __versiondatetime__
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_moneymoney.settings')
-
+print("Checking database version")
+con=Connection()
+con.db=settings.DATABASES['default']['NAME']
+con.password=settings.DATABASES['default']['PASSWORD']
+con.port=settings.DATABASES['default']['PORT']
+con.server=settings.DATABASES['default']['HOST']
+con.user=settings.DATABASES['default']['USER']
+con.connect()
+database_update(con, 'moneymoney', __versiondatetime__,"Console")
+con.disconnect()
 application = get_asgi_application()
