301265510c761d93dd931fa309844d0cd6b94bb4
turulomio <turulomio@yahoo.es>
2021-10-20 04:54:04 +0200

    Updated functions. Added investment_operations simulation capabilities

--- moneymoney/__init__.py
+++ moneymoney/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 __version__="0.1.0"
-__versiondatetime__= datetime(2021, 10, 18, 8, 41)
+__versiondatetime__= datetime(2021, 10, 20, 4, 47)
 __versiondate__=__versiondatetime__.date()
