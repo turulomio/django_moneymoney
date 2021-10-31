304f282e8f2e3c2bf93974b7672ab46f7c6257cb
turulomio <turulomio@yahoo.es>
2021-10-31 07:43:00 +0100

    Added stockmarkets table

--- moneymoney/__init__.py
+++ moneymoney/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
 __version__="0.1.0"
-__versiondatetime__= datetime(2021, 10, 24, 6, 18)
+__versiondatetime__= datetime(2021, 10, 31, 7, 5)
 __versiondate__=__versiondatetime__.date()
--- new_installation.py
+++
@@ -1,4 +0,0 @@
-
-dropdb -U postgres moneymoney_deleteme; createdb -U postgres moneymoney_deleteme
-python manage.py database_update --settings django_moneymoney.presettings
-
