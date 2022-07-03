67caf81d2a5cc62a13d2f9956e085b4b4a430a3e
turulomio <turulomio@yahoo.es>
2022-07-03 07:08:34 +0200

    django_moneymoney-0.3.0

--- moneymoney/__init__.py
+++ moneymoney/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="0.2.0"
-__versiondatetime__= datetime(2022, 7, 3, 6, 26)
+__version__="0.3.0"
+__versiondatetime__= datetime(2022, 7, 3, 7, 5)
 __versiondate__=__versiondatetime__.date()
