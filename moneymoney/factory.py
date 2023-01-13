961adde724254d0cffa5f5589dbdc16576302134
turulomio <turulomio@yahoo.es>
2023-01-13 14:28:05 +0100

    Improving tests with factory-boy

---
+++ moneymoney/factory.py
@@ -0,0 +1,23 @@
+from factory import Faker, SubFactory
+from factory.django import DjangoModelFactory
+from moneymoney import models
+#https://faker.readthedocs.io/en/master/providers/faker.providers.currency.html
+
+class BanksFactory(DjangoModelFactory):
+    class Meta:
+        model= models.Banks
+        
+    name = Faker("company")
+    active = Faker("boolean")
+        
+class AccountsFactory(DjangoModelFactory):
+    class Meta:
+        model= models.Accounts
+        
+    name = Faker("name")
+    active = Faker("boolean")
+    banks=SubFactory(BanksFactory)
+    number="1223412"
+    currency="EUR"
+    decimals=2
+
