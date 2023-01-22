5a5b5d3f772c30eea81cb73e8c108c5a2a2e8b06
turulomio <turulomio@yahoo.es>
2023-01-22 11:23:09 +0100

    testing functions

--- moneymoney/tests.py
+++ moneymoney/tests.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.test import tag
+from django.utils import timezone
 from json import loads
 from moneymoney import models, factory, factory_helpers 
+from moneymoney.reusing.connection_dj import cursor_one_row
 from moneymoney.types import eOperationType
-
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 from django.contrib.auth.models import Group
@@ -176,10 +177,6 @@ class CtTestCase(APITestCase):
         r=self.client_authorized_1.post(mf.url, mf.post_payload(currency="EUR"))
         self.assertEqual(r.status_code, status.HTTP_201_CREATED)
         
-        #List accounts with balance
-        r=self.client_authorized_1.get(mf.url+"withbalance/")
-        self.assertEqual(r.status_code, status.HTTP_200_OK)
-        
         #Create a new account operation
         r=self.client_authorized_1.post(mfao.url, mfao.post_payload(accounts__currency="EUR", amount=-1492,  comment="CAN YOU FIND ME?", concepts=models.Concepts.objects.get(pk=7), accounts=models.Accounts.objects.get(pk=4)))
         self.assertEqual(r.status_code, status.HTTP_201_CREATED)
@@ -189,16 +186,25 @@ class CtTestCase(APITestCase):
         r=self.client_authorized_1.get(mfao.url+"?search=FIND ME")
         self.assertEqual(r.status_code, status.HTTP_200_OK)
                 
+        #List accounts with balance
+        r=self.client_authorized_1.get(mf.url+"withbalance/")
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        accounts=loads(r.content)
+        self.assertEqual(accounts[0]["balance_account"], -1492)
+        
+        account_balance=cursor_one_row("select * from account_balance(%s,%s,%s)", (4, timezone.now(), 'EUR'))
+        self.assertEqual(account_balance["balance_account_currency"], -1492)
+        accounts_balance=cursor_one_row("select * from accounts_balance(%s,%s)", (timezone.now(), 'EUR'))
+        self.assertEqual(accounts_balance["accounts_balance"], -1492)
+        total_balance=cursor_one_row("select * from total_balance(%s,%s)", (timezone.now(), 'EUR'))
+        self.assertEqual(total_balance["total_user"], -1492)
+        
         # Gets annual reports
         year=int(ao["datetime"][0:4])
-        month=int(ao["datetime"][5:7])
-        print(year, month)
+#        month=int(ao["datetime"][5:7])
         r=self.client_authorized_1.get(f"/reports/annual/{year}/")
         total=loads(r.content)
-        print(ao)
         print(total)
         r=self.client_authorized_1.get(f"/reports/annual/income/{year}/")
         total=loads(r.content)
-        print(ao)
         print(total)
-
