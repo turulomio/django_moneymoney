80471f6c15b8fede7d948f02a7ce40091c7b84f0
turulomio <turulomio@yahoo.es>
2023-03-22 11:16:39 +0100

    Added internet load_catalogs

--- moneymoney/management/commands/loaddata_catalogs.py
+++ moneymoney/management/commands/loaddata_catalogs.py
@@ -1,17 +1,25 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from django.core.management import call_command
+from requests import get
 
 
 
 class Command(BaseCommand):
     help = 'Load two fixtures data'
         #Generate fixtures
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument('--internet', action='store_true', help='Load data from internet', default=False)
                 
     def handle(self, *args,**options):
-
-        call_command(
-            "loaddata",
-            "moneymoney/fixtures/all.json", 
-        )
+        if options["internet"]:
+            r=get("https://github.com/turulomio/django_moneymoney/raw/main/moneymoney/fixtures/all.json")
+            filename=f"{settings.TMPDIR}/all.json"
+            with open(filename, "wb") as f:
+                f.write(r.content)
+        else:
+            filename="moneymoney/fixtures/all.json"
+        call_command("loaddata", filename )
         
