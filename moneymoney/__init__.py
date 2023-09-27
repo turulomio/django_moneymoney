1a4f337e006930757ea3a679d111813d86664b09
turulomio <turulomio@yahoo.es>
2023-09-27 04:44:05 +0200

    django_moneymoney-1.1.0

--- moneymoney/__init__.py
+++ moneymoney/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="1.0.0"
-__versiondatetime__= datetime(2023, 9, 9, 5, 43)
+__version__="1.1.0"
+__versiondatetime__= datetime(2023, 9, 27, 4, 43)
 __versiondate__=__versiondatetime__.date()
