2b833b276efa8439666c759b6cc1619b0d48b3bb
turulomio <turulomio@yahoo.es>
2024-03-16 06:15:57 +0100

    Improving tests

--- moneymoney/tests.py
+++ moneymoney/tests.py
@@ -8,6 +8,7 @@ from moneymoney import models, ios, investing_com
 from moneymoney.reusing import tests_helpers
 from os import path
 from pydicts import lod
+from request_casting.request_casting import id_from_url
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
 from django.contrib.auth.models import Group
@@ -145,18 +146,18 @@ class CtTestCase(APITestCase):
         
         tests_helpers.client_get(self, self.client_authorized_1, "/api/accountsoperations/", status.HTTP_200_OK)
         self.assertEqual(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).count(), 3)
-        self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [1, 2, 3 ])   
-        self.assertEqual(models.Accountsoperations.objects.get(pk=1).amount, -1000)
-        self.assertEqual(models.Accountsoperations.objects.get(pk=2).amount, 1000)
-        self.assertEqual(models.Accountsoperations.objects.get(pk=3).amount, -10)
+        self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [id_from_url(dict_transfer["ao_origin"]), id_from_url(dict_transfer["ao_destiny"]), id_from_url(dict_transfer["ao_commission"])])
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_origin"])).amount, -1000)
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_destiny"])).amount, 1000)
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_commission"])).amount, -10)
         
         # Update transfer
-        tests_helpers.client_put(self, self.client_authorized_1, dict_transfer["url"],  models.Accountstransfers.post_payload(datetime=timezone.now(), amount=999, commission=9), status.HTTP_200_OK)
-        self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [4, 5, 6])   
-        self.assertEqual(models.Accountsoperations.objects.filter(pk__in=[1, 2, 3]).count(), 0)
-        self.assertEqual(models.Accountsoperations.objects.get(pk=4).amount, -999)
-        self.assertEqual(models.Accountsoperations.objects.get(pk=5).amount, 999)
-        self.assertEqual(models.Accountsoperations.objects.get(pk=6).amount, -9)
+        dict_transfer_updated=tests_helpers.client_put(self, self.client_authorized_1, dict_transfer["url"],  models.Accountstransfers.post_payload(datetime=timezone.now(), amount=999, commission=9), status.HTTP_200_OK)
+        self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [id_from_url(dict_transfer_updated["ao_origin"]), id_from_url(dict_transfer_updated["ao_destiny"]), id_from_url(dict_transfer_updated["ao_commission"])])   
+        self.assertEqual(models.Accountsoperations.objects.filter(pk__in=[id_from_url(dict_transfer["ao_origin"]), id_from_url(dict_transfer["ao_destiny"]), id_from_url(dict_transfer["ao_commission"])]).count(), 0)
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_origin"])).amount, -999)
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_destiny"])).amount, 999)
+        self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_commission"])).amount, -9)
     
         # Delete transfer
         self.client_authorized_1.delete(dict_transfer["url"])
