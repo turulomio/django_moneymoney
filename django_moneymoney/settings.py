f7400d5b14cb5278688dfdd38f3d4a60f3731d11
turulomio <turulomio@yahoo.es>
2024-03-28 21:35:23 +0100

    Github actions for django test works fine (#105)
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying
    
    * Trying

--- django_moneymoney/settings.py
+++ django_moneymoney/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'xulpymoney',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
