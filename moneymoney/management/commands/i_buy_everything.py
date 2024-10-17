ac585db3883f86d6e936c1deae6618f753cf31a5
turulomio <turulomio@yahoo.es>
2024-10-17 23:03:51 +0200

    Seems to work

--- moneymoney/management/commands/i_buy_everything.py
+++ moneymoney/management/commands/i_buy_everything.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
             Esta función solicita al usuario que ingrese tres números, calcula su promedio  y muestra el resultado en la consola.
         """
 
-#            inicio = float(input("Ingresa el primer precio de compra: "))
+#            primera_capa_alcista = float(input("Ingresa el primer precio de compra: "))
 #            alcista = bool(input("Ingresa 1 si es alcista y si no 0"))
 #            maximo = float(input("Ingresa el segundo número: "))
 #            minimo = float(input("Ingresa el tercer número: "))
@@ -16,43 +16,45 @@ class Command(BaseCommand):
         
         apalancamiento=20
             
-        inicio=20440 
-        alcista=True
-        capas_mal=5
+        primera_capa_alcista=20440 
+        capas_alcistas=9
+        capas_bajistas=8
         
-        minimo=inicio-10*capas_mal
+        minimo=primera_capa_alcista-10*capas_bajistas
+        maximo=primera_capa_alcista+10*(capas_alcistas-1)
         
+        print ("Estudio en dolares)")
         
-        minimo=20360
+        print("Capa primera_capa_alcista", primera_capa_alcista,  "US100",  "Capas alcistas",  capas_alcistas,  "Capas bajistas",  capas_bajistas)
+        print("Minimo", minimo)
+        print("Maximo",  maximo)
+                
+        for precio_venta in range(primera_capa_alcista,30000, 10):
+            ganancia_total=self.ganancia(precio_venta, primera_capa_alcista, precio_venta,  apalancamiento)-self.ganancia(precio_venta, minimo, primera_capa_alcista , apalancamiento)#alcista - bajista
+            if ganancia_total>10:
+                print("Ganancia total bien en ",  precio_venta,  "=",  ganancia_total,  "$")
+                print("Capas desde primera_capa_alcista", self.capas_entre(primera_capa_alcista, precio_venta))
+                break
 
-        print("Capa inicio", inicio, "Alcista" if alcista else "Bajista", "US100")
-#        print("Capas por arriba", (maximo-inicio)/10, "hasta", maximo)
-        print("Capas por debajo", (inicio-minimo)/10, "hasta", minimo)
+        print()
+        print("ESTUDIO MAL")        
+        for precio_venta in range(primera_capa_alcista,0, -10):
+            ganancia_total=-self.ganancia(precio_venta, primera_capa_alcista, maximo,  apalancamiento)+self.ganancia(precio_venta, precio_venta, primera_capa_alcista-10 , apalancamiento)#alcista - bajista .-10 porque empieza en alcista
+            if ganancia_total>10:
+                print("Ganancia total malen ",  precio_venta,  "=",  ganancia_total,  "$")
+                print("Capas desde primera_capa_alcista", self.capas_entre(primera_capa_alcista, precio_venta))
+                break
 
-        acciones=(20520-20360)/10/100+0.01
+
+    def capas_entre(self, a,  b):
+        return int(abs(a-b)/10)
         
         
-        print("Acciones totales invertidas", acciones)
-        
-        invertido=0
-#        for capa in range (minimo, maximo+10, 10):
-#            print(capa)
-        
-#            invertido+= 0.01*apalancamiento*capa
-        
-#        print("Importe invertido", invertido)
-        
-        capa=20360
-        gananciatotal=0
-        precio_actual_alcista=inicio
-        for precio_actual in range(inicio,30000, 10):
-             gananciatotal+=(precio_actual_alcista-capa)*0.01*apalancamiento
-#                 for capa in range (minimo, maximo+10, 10):
-                    
-        
-            capa+=10
-            precio_actual_alcista+=10
-            
-    def ganancia_bajista(minimo, precio): 
+    def ganancia(self,  precio,  capa_baja,  capa_alta, apalancamiento):
+        r=0
+        for capa in range(capa_baja,  capa_alta+10,  10):
+            r+=abs(precio-capa)*0.01*apalancamiento
+#        print("Ganancia en ", precio,  capa_baja,  capa_alta,  "es",   r)
+        return r
         
 
