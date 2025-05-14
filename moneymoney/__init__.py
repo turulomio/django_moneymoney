296fc7dafb5dad698caef9bf40f90fc9c2a12843
turulomio <turulomio@yahoo.es>
2025-05-14 18:56:11 +0200

    django_moneymoney-1.3.0 (#156)
    * django_moneymoney-1.3.0
    
    * poetry update 0
    
    * Updated version in pyproject

--- moneymoney/__init__.py
+++ moneymoney/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="1.2.0"
-__versiondatetime__= datetime(2023, 10, 5, 7, 54)
+__version__="1.3.0"
+__versiondatetime__= datetime(2025, 5, 14, 18, 30)
 __versiondate__=__versiondatetime__.date()
