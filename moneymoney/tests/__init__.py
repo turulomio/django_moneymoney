8d318c282dcbbcedab2d5d2e8c2e2255a86220e6
turulomio <turulomio@yahoo.es>
2025-12-18 09:59:50 +0100

    Tests splitted. Now you can update commission to 0 in accounts transfers (#209)
    * Preparing to split tests
    
    * Trying
    
    * Splittting
    
    * Almost
    
    * All test work+
    
    * Now works updating

---
+++ moneymoney/tests/__init__.py
@@ -0,0 +1,104 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
+from moneymoney.reusing.tests_dinamic_methods import add_method_to_this_class_dinamically
+from moneymoney import types
+from datetime import date, timedelta, datetime
+from django.db import  connection
+from django.test.utils import CaptureQueriesContext
+from logging import getLogger, ERROR
+from pydicts import casts
+logger = getLogger('django.request')
+logger.setLevel(ERROR) # This will suppress INFO and WARNING
+
+class assert_max_queries(CaptureQueriesContext):
+    def __init__(self, test_case, max_queries):
+        self.test_case = test_case
+        self.max_queries = max_queries
+        super().__init__(connection)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        super().__exit__(exc_type, exc_value, traceback)
+        if exc_type is not None:
+            return
+
+        num_queries = len(self)
+
+        if num_queries > self.max_queries:
+            total_time = 0
+            strqueries = ""
+            for query in self.captured_queries:
+                total_time += float(query['time'])
+                strqueries += f"[{query['time']}s] {query['sql']}\n\n"
+
+            msg = (
+                f"Queries:\n{strqueries}"
+                f"Exceeded query limit. Executed {num_queries} queries in {total_time:.3f}s, "
+                f"but the limit was {self.max_queries}.\n"
+            )
+            self.test_case.fail(msg)
+
+class MoneyMoneyAPITestCase(APITestCase):
+    """
+    Base class for API tests.
+    It sets up fixtures and authenticated clients once for all inheriting test classes.
+    """
+    fixtures = ["all.json","test_users.json"]
+    js_image_b64="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII="
+
+    timezone_madrid="Europe/Madrid"
+    now=datetime.now()
+    today=date.today()
+    today_year=today.year
+    today_month=today.month        
+
+    yesterday=date.today()-timedelta(days=1)
+    yesterday_year=yesterday.year
+    yesterday_month=yesterday.month
+
+    dtaware_last_year=casts.dtaware_year_end(today_year-1, timezone_madrid)
+    dtaware_now=casts.dtaware_now()
+    dtaware_yesterday=dtaware_now-timedelta(days=1)
+
+    # Defines report moment for static reports to avoid problems with asserts
+    static_year=2024
+    static_month=1
+
+    # Concepts url
+    hurl_concepts_oa=f"http://testserver/api/concepts/{types.eConcept.OpenAccount}/"
+    hurl_concepts_fo=f"http://testserver/api/concepts/{types.eConcept.FastInvestmentOperations}/"
+
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs )   
+        # add_methodtypes_to_this_object_dinamically(self)
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up non-database specific resources once for all test classes.
+        """
+        super().setUpClass()
+
+
+        cls.user_authorized_1 = User.objects.get(username='authorized_1')
+        cls.user_authorized_2 = User.objects.get(username='authorized_2')
+        cls.user_catalog_manager = User.objects.get(username='catalog_manager')
+
+        cls.client_authorized_1 = APIClient()
+        cls.client_authorized_1.force_authenticate(user=cls.user_authorized_1)
+        cls.client_authorized_1.user = cls.user_authorized_1
+
+        cls.client_authorized_2 = APIClient()
+        cls.client_authorized_2.force_authenticate(user=cls.user_authorized_2)
+        cls.client_authorized_2.user = cls.user_authorized_2
+
+        cls.client_anonymous = APIClient()
+        cls.client_anonymous.user = None
+
+        cls.client_catalog_manager = APIClient()
+        cls.client_catalog_manager.force_authenticate(user=cls.user_catalog_manager)
+        cls.client_catalog_manager.user = cls.user_catalog_manager
+
+
+add_method_to_this_class_dinamically(MoneyMoneyAPITestCase, "moneymoney/tests", "test_*.py")
---
+++ moneymoney/tests/test_accountstransfers.py
@@ -0,0 +1,43 @@
+from django.utils import timezone
+from moneymoney import models, functions
+
+from request_casting.request_casting import id_from_url
+from moneymoney.reusing import tests_helpers
+from moneymoney.tests import assert_max_queries
+from rest_framework import status
+
+def test_Accountstransfers(self):      
+    tests_helpers.client_get(self, self.client_authorized_1, "/api/accounts/4/", status.HTTP_200_OK)
+    dict_destiny=tests_helpers.client_post(self, self.client_authorized_1, "/api/accounts/",  models.Accounts.post_payload(), status.HTTP_201_CREATED)
+
+    # Create transfer  
+    with assert_max_queries(self, 47):
+        dict_transfer=tests_helpers.client_post(self, self.client_authorized_1, "/api/accountstransfers/",  models.Accountstransfers.post_payload(destiny=dict_destiny["url"]), status.HTTP_201_CREATED)
+    with assert_max_queries(self,2):
+        tests_helpers.client_get(self, self.client_authorized_1, "/api/accountsoperations/", status.HTTP_200_OK)
+    self.assertEqual(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).count(), 3)
+    self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [id_from_url(dict_transfer["ao_origin"]), id_from_url(dict_transfer["ao_destiny"]), id_from_url(dict_transfer["ao_commission"])])
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_origin"])).amount, -1000)
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_destiny"])).amount, 1000)
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer["ao_commission"])).amount, -10)
+        
+    # Update transfer
+    dict_transfer_updated=tests_helpers.client_put(self, self.client_authorized_1, dict_transfer["url"],  models.Accountstransfers.post_payload(datetime=timezone.now(), amount=999, commission=9, destiny=dict_destiny["url"]), status.HTTP_200_OK)
+    self.assertEqual(list(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).values_list("id",  flat=True)), [id_from_url(dict_transfer_updated["ao_origin"]), id_from_url(dict_transfer_updated["ao_destiny"]), id_from_url(dict_transfer_updated["ao_commission"])])   
+    
+    # Check occounts ids changed after update
+    self.assertEqual(models.Accountsoperations.objects.filter(pk__in=[id_from_url(dict_transfer["ao_origin"]), id_from_url(dict_transfer["ao_destiny"]), id_from_url(dict_transfer["ao_commission"])]).count(), 0)
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_origin"])).amount, -999)
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_destiny"])).amount, 999)
+    self.assertEqual(models.Accountsoperations.objects.get(pk=id_from_url(dict_transfer_updated["ao_commission"])).amount, -9)
+
+    # Delete transfer
+    self.client_authorized_1.delete(dict_transfer["url"])
+    with self.assertRaises(models.Accountstransfers.DoesNotExist):
+        models.Accountstransfers.objects.get(id=dict_transfer["id"])
+    self.assertEqual(models.Accountsoperations.objects.filter(associated_transfer__id=dict_transfer["id"]).count(), 0)
+
+    # Deleting a commission after create transfer doesn't crash
+    dict_transfer=tests_helpers.client_post(self, self.client_authorized_1, "/api/accountstransfers/",  models.Accountstransfers.post_payload(destiny=dict_destiny["url"]), status.HTTP_201_CREATED)
+    dict_transfer_updated=tests_helpers.client_put(self, self.client_authorized_1, dict_transfer["url"],  models.Accountstransfers.post_payload(datetime=timezone.now(), amount=999, commission=0, destiny=dict_destiny["url"]), status.HTTP_200_OK)
+    self.assertEqual(dict_transfer_updated["ao_commission"], None)
