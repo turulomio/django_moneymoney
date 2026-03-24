4879c74e9e911c970fc40788c21f828ba19c90a4
turulomio <turulomio@yahoo.es>
2026-03-24 07:36:14 +0100

    Fixed bug with default factors values

--- moneymoney/tests/test_accountsoperations.py
+++ moneymoney/tests/test_accountsoperations.py
@@ -1,15 +1,17 @@
 from moneymoney import models
 from moneymoney.reusing import tests_helpers
+from moneymoney.functions import print_object
 from rest_framework import status
 from pydicts import lod
 from django.utils import timezone
 from moneymoney import types
 from moneymoney.tests import assert_max_queries
+from django.test import tag
+from datetime import timedelta
+from decimal import Decimal
 
 
-
-
-def test_Accountsoperations(self):
+def test_accountsoperations_model(self):
     o=models.Accountsoperations()
     o.accounts_id=4
     o.amount=-1000
@@ -27,8 +29,7 @@ def test_Accountsoperations(self):
 
 
 
-
-def test_Accountsoperations_refunds(self):
+def test_accountsoperations_refunds(self):
     #Adding an ao
     dict_ao=tests_helpers.client_post(self, self.client_authorized_1, "/api/accountsoperations/",  models.Accountsoperations.post_payload(concepts=f"/api/concepts/{types.eConcept.BankCommissions}/", amount=-1000), status.HTTP_201_CREATED)
     
@@ -50,17 +51,23 @@ def test_Accountsoperations_refunds(self):
     # Set a too much high refund
     tests_helpers.client_post(self, self.client_authorized_1, dict_ao["url"]+"create_refund/", {"datetime": timezone.now(), "refund_amount":2000, "comment": "Too much"} , status.HTTP_400_BAD_REQUEST)
 
+@tag("current")
+def test_accountsoperations_different_currency(self):
+    # Creates a new account with USD
+    account_usd=models.Accounts.objects.create(name="New account", banks_id=3, active=True, decimals=2, currency="USD")
+    ao_usd=models.Accountsoperations.objects.create(accounts=account_usd, amount=-1000, datetime=timezone.now(), concepts_id=types.eConcept.BankCommissions)
+    self.assertEqual(ao_usd.accounts.currency, "USD")
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="EUR")["balance_account_currency"],-1000)
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="EUR")["balance_user_currency"],-1000)
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="USD")["balance_account_currency"],-1000)
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="USD")["balance_user_currency"],-1000)
 
-def test_Accountsoperations_associated_fields(self):
-    #Add a investment operation to check associated_io
-    tests_helpers.client_post(self, self.client_authorized_1, "/api/quotes/",  models.Quotes.post_payload(), status.HTTP_201_CREATED)
-    dict_investment=tests_helpers.client_post(self, self.client_authorized_1, "/api/investments/", models.Investments.post_payload(), status.HTTP_201_CREATED)        
-    dict_io=tests_helpers.client_post(self, self.client_authorized_1, "/api/investmentsoperations/", models.Investmentsoperations.post_payload(investments=dict_investment["url"]), status.HTTP_201_CREATED)#Al actualizar ao asociada ejecuta otro plio
-    dict_associated_ao_with_associated_io=tests_helpers.client_get(self, self.client_authorized_1, dict_io["associated_ao"],  status.HTTP_200_OK)
-    self.assertEqual(dict_associated_ao_with_associated_io["associated_io"], dict_io["url"])
-    
-    #Add a dividend to check associated_dividend
-    dict_dividend=tests_helpers.client_post(self, self.client_authorized_1, "/api/dividends/",  models.Dividends.post_payload(investments=dict_investment["url"]), status.HTTP_201_CREATED)
-    dict_associated_ao_with_associated_dividend=tests_helpers.client_get(self, self.client_authorized_1, dict_dividend["accountsoperations"],  status.HTTP_200_OK)
-    self.assertEqual(dict_associated_ao_with_associated_dividend["associated_dividend"], dict_dividend["url"])
-    
+    # Creates a new quote with EURUSD yesterday
+    models.Quotes.objects.create(products_id=74747, datetime=timezone.now()-timedelta(days=1), quote=1.1)
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="EUR")["balance_account_currency"],-1000) #USD
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="EUR")["balance_user_currency"], Decimal('-909.0909090909090909090909091') ) #EUR
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="USD")["balance_account_currency"],-1000) #USD
+    self.assertEqual(account_usd.balance(timezone.now(),currency_user="USD")["balance_user_currency"],-1000) #USD
+
+
+
